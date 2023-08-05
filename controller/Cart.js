260f52d9e90e99f0cf2ef8631db488f929232373
Abhias2405 <abhias2405@gmail.com>
2023-08-05 08:51:25 +0530

    checking

--- controller/Cart.js
+++ controller/Cart.js
@@ -1,26 +1,11 @@
 const { Cart } = require('../model/Cart');
-const { Product } = require('../model/Product');
 
 exports.fetchCartByUser = async (req, res) => {
   const { id } = req.user;
   try {
     const cartItems = await Cart.find({ user: id }).populate('product');
 
-    // Calculate and add the discount price to each product in the cart
-    const cartItemsWithPrices = cartItems.map((cartItem) => {
-      const { product, quantity } = cartItem;
-      const discountPrice = Math.round(product.price * (1 - product.discountPercentage / 100));
-      return {
-        ...cartItem.toJSON(),
-        product: {
-          ...product.toJSON(),
-          discountPrice,
-        },
-        subtotal: discountPrice * quantity,
-      };
-    });
-
-    res.status(200).json(cartItemsWithPrices);
+    res.status(200).json(cartItems);
   } catch (err) {
     res.status(400).json(err);
   }
@@ -32,20 +17,7 @@ exports.addToCart = async (req, res) => {
   try {
     const doc = await cart.save();
     const result = await doc.populate('product').execPopulate();
-
-    // Calculate and add the discount price to the product in the cart
-    const { product, quantity } = result;
-    const discountPrice = Math.round(product.price * (1 - product.discountPercentage / 100));
-    const cartItemWithPrices = {
-      ...result.toJSON(),
-      product: {
-        ...product.toJSON(),
-        discountPrice,
-      },
-      subtotal: discountPrice * quantity,
-    };
-
-    res.status(201).json(cartItemWithPrices);
+    res.status(201).json(result);
   } catch (err) {
     res.status(400).json(err);
   }
@@ -68,20 +40,7 @@ exports.updateCart = async (req, res) => {
       new: true,
     });
     const result = await cart.populate('product').execPopulate();
-
-    // Calculate and add the discount price to the product in the cart
-    const { product, quantity } = result;
-    const discountPrice = Math.round(product.price * (1 - product.discountPercentage / 100));
-    const cartItemWithPrices = {
-      ...result.toJSON(),
-      product: {
-        ...product.toJSON(),
-        discountPrice,
-      },
-      subtotal: discountPrice * quantity,
-    };
-
-    res.status(200).json(cartItemWithPrices);
+    res.status(200).json(result);
   } catch (err) {
     res.status(400).json(err);
   }
