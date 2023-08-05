e9d83991e2997212b2ad0d2e6490b59ea2f2e55c
Abhias2405 <abhias2405@gmail.com>
2023-08-05 09:11:07 +0530

    ith

--- controller/Cart.js
+++ controller/Cart.js
@@ -1,4 +1,5 @@
 const { Cart } = require('../model/Cart');
+const { Product } = require('../model/Product');
 
 exports.fetchCartByUser = async (req, res) => {
   const { id } = req.user;
@@ -6,10 +7,10 @@ exports.fetchCartByUser = async (req, res) => {
     const cartItems = await Cart.find({ user: id }).populate('product');
 
     // Calculate and add the discount price to each cart item
-    const cartItemsWithPrices = cartItems.map((cartItem) => ({
-      ...cartItem.toJSON(),
-      discountPrice: Math.round(cartItem.product.price * (1 - cartItem.product.discountPercentage / 100)),
-    }));
+    const cartItemsWithPrices = cartItems.map((cartItem) => {
+      const discountPrice = Math.round(cartItem.product.price * (1 - cartItem.product.discountPercentage / 100));
+      return { ...cartItem.toJSON(), discountPrice };
+    });
 
     res.status(200).json(cartItemsWithPrices);
   } catch (err) {
@@ -19,11 +20,19 @@ exports.fetchCartByUser = async (req, res) => {
 
 exports.addToCart = async (req, res) => {
   const { id } = req.user;
-  const cart = new Cart({ ...req.body, user: id });
   try {
+    const product = await Product.findById(req.body.product);
+    if (!product) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+
+    const cart = new Cart({ ...req.body, user: id });
+    const discountPrice = Math.round(product.price * (1 - product.discountPercentage / 100));
+    cart.discountPrice = discountPrice;
+
     const doc = await cart.save();
     const result = await doc.populate('product').execPopulate();
-    result.discountPrice = Math.round(result.product.price * (1 - result.product.discountPercentage / 100));
+
     res.status(201).json(result);
   } catch (err) {
     res.status(400).json(err);
@@ -43,11 +52,17 @@ exports.deleteFromCart = async (req, res) => {
 exports.updateCart = async (req, res) => {
   const { id } = req.params;
   try {
-    const cart = await Cart.findByIdAndUpdate(id, req.body, {
-      new: true,
-    });
+    const product = await Product.findById(req.body.product);
+    if (!product) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+
+    const cart = await Cart.findByIdAndUpdate(id, req.body, { new: true });
+    const discountPrice = Math.round(product.price * (1 - product.discountPercentage / 100));
+    cart.discountPrice = discountPrice;
+
     const result = await cart.populate('product').execPopulate();
-    result.discountPrice = Math.round(result.product.price * (1 - result.product.discountPercentage / 100));
+
     res.status(200).json(result);
   } catch (err) {
     res.status(400).json(err);
